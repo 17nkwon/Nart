f5f98cd69bf736b7908f31a671d3865191e1662c
Nana Kwon <17nkwon@gmail.com>
2021-05-16 07:57:33 -0400

    add code to github

---
+++ frontend/src/data.js
@@ -0,0 +1,66 @@
+const data = {
+    products:[
+        {
+            id: '1',
+            name: 'Hanging Plants',
+            category: 'Plants',
+            image: '/images/product1.jpg',
+            price: 2.00,
+            rating: 0,
+            numReviews: 0,
+            description: 'three plants hanging',
+        },
+        {
+            id: '2',
+            name: 'Love Me Daisy',
+            category: 'Plants',
+            image: '/images/product2.jpg',
+            price: 2.00,
+            rating: 0,
+            numReviews: 0,
+            description: 'daisy with the words love me or love me not',
+        },
+        {
+            id: '3',
+            name: 'Tree',
+            category: 'Plants',
+            image: '/images/product3.jpg',
+            price: 2.00,
+            rating: 0,
+            numReviews: 0,
+            description: 'tree in a box',
+        },
+        {
+            id: '4',
+            name: 'Lit Outfit',
+            category: 'Characters',
+            image: '/images/product4.jpg',
+            price: 2.00,
+            rating: 0,
+            numReviews: 0,
+            description: 'girl with a flame skirt and blue background',
+        },
+        {
+            id: '5',
+            name: 'Space Girl',
+            category: 'Characters',
+            image: '/images/product5.jpg',
+            price: 2.00,
+            rating: 0,
+            numReviews: 0,
+            description: 'girl with space buns and galaxy background',
+        },
+        {
+            id: '6',
+            name: 'Star Girl',
+            category: 'Characters',
+            image: '/images/product6.jpg',
+            price: 2.00,
+            rating: 0,
+            numReviews: 0,
+            description: 'mystical girl with a star necklace and star background',
+        }
+    ]
+};
+
+export default data;
