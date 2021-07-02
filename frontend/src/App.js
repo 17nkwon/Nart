abe874a99dedc30694c59b23f3b67fa60b21b3a2
Nana Kwon <17nkwon@gmail.com>
2021-07-02 17:12:10 -0400

    product pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,28 @@
 import React from 'react';
-import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './pages/HomeScreen';
+import ProductScreen from './pages/ProductScreen';
 
 function App() {
   return (
-    
+    <BrowserRouter>
     <div className="grid-container">
-            <header className="row">
-                <div>
-                    <a className="brand" href="/">NART</a>
-                </div>
-                <div>
-                    <a href="/cart">Cart</a>
-                    <a href="/signin">Sign in</a>
-                </div>
-            </header>
-            <main>
-                <div className="row center">
-                  {
-                    data.products.map(product => (
-                      <div key={product.id} className="card">
-                        <a href={`/product/${product.id}`}>
-                          <img className="medium" src={product.image} alt={product.name} /></a>
-                        <div className="card-body">
-                            <a href="product.html"><h2>{product.name}</h2></a>
-                            <div className="rating">
-                                <span><i className="fa fa-star"></i></span>
-                                <span><i className="fa fa-star"></i></span>
-                                <span><i className="fa fa-star"></i></span>
-                                <span><i className="fa fa-star"></i></span>
-                                <span><i className="fa fa-star"></i></span>
-                            </div>
-                            <div className="price">${product.price}</div>
-                        </div>
-                    </div>
-                    ))
-                  }
-                </div>
-            </main>
-            <footer className="row center">
-                All rights reserved.
-            </footer>
+      <header className="row">
+        <div>
+          <a className="brand" href="/">NART</a>
         </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign in</a>
+        </div>
+      </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+      </main>
+      <footer className="row center">All rights reserved.</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
