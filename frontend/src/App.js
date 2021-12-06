fb5702bc6cf962d6884fe7b8a1c0901b4d1aac20
Nana Kwon <17nkwon@gmail.com>
2021-12-06 10:38:55 -0500

    signin action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { signout } from './actions/userActions';
 import CartScreen from './pages/CartScreen';
 import HomeScreen from './pages/HomeScreen';
 import ProductScreen from './pages/ProductScreen';
+import SigninScreen from './pages/SigninScreen';
 
 function App() {
   const cart = useSelector(state => state.cart);
   const { cartItems } = cart;
+  const userSignin = useSelector(state => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  }
+
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -21,18 +30,28 @@ function App() {
                 <span className="badge">{cartItems.length}</span>
               )}
             </Link>
-            <Link to="/signin">Sign in</Link>
+            {
+              userInfo ? (
+                <div className="dropdown">
+                  <Link to="#">{userInfo.name} <i className="fa fa-caret-down"></i></Link>
+                  <ul className="dropdown-content">
+                    <Link to="#signout" onClick={signoutHandler}>Sign Out</Link></ul></div>
+              ) : (
+                <Link to="/signin">Sign in</Link>
+              )}
+
           </div>
         </header>
         <main>
           {/*the question mark makes having the id optional so when the user directly goes to the cart it doesn't add a new item*/}
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All rights reserved.</footer>
-      </div>
-    </BrowserRouter>
+      </div >
+    </BrowserRouter >
   );
 }
 
